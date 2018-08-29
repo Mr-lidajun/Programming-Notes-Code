4dbc441d9ffc0cc15f6468b17062889b9bc10ed8
lidajun828 <!QAZ2wsx123456>
2018-08-29 18:03:18 +0800

    ﻿2.3.3 Binder

---
+++ chapter_2/src/main/aidl/com/ldj/chapter_2/aidl/Book.aidl
@@ -0,0 +1,6 @@
+// Book.aidl
+package com.ldj.chapter_2.aidl;
+
+// Declare any non-default types here with import statements
+
+parcelable Book;
---
+++ chapter_2/src/main/java/com/ldj/chapter_2/UserManager.java
@@ -0,0 +1,11 @@
+package com.ldj.chapter_2;
+
+/**
+ *
+ * @author lidajun
+ * @date 2018/8/29
+ */
+
+public class UserManager {
+    public static int sUserId = 1;
+}
