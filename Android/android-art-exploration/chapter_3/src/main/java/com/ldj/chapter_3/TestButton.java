5b0b4a87fbab514f2cd379c79e1ff157eb7e333d
Mr_lidajun <solidajun@gmail.com>
2018-10-10 00:20:39 +0800

    3.2.4　各种滑动方式的对比-演示采用动画的方式实现全屏滑动

--- Android/android-art-exploration/chapter_3/src/main/java/com/ldj/chapter_3/TestButton.java
+++ Android/android-art-exploration/chapter_3/src/main/java/com/ldj/chapter_3/TestButton.java
@@ -2,7 +2,11 @@ package com.ldj.chapter_3;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
 import android.widget.TextView;
 
 /**
@@ -31,6 +35,33 @@ public class TestButton extends TextView {
 
 
     private void init() {
+        mScaledTouchSlop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
+        Log.d(TAG, "sts:" + mScaledTouchSlop);
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getRawX();
+        int y = (int) event.getRawY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = x - mLastX;
+                int deltaY = y - mLastY;
+                int translationX = (int) (this.getTranslationX() + deltaX);
+                int translationY = (int) (this.getTranslationY() + deltaY);
+                this.setTranslationX(translationX);
+                this.setTranslationY(translationY);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                break;
+        }
+
+        mLastX = x;
+        mLastY = y;
+        return true;
     }
 }
