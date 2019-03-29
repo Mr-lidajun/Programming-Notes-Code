de480345fd2d1525ef7558d444f603cb078a2ccd
lidajun828 <!QAZ2wsx123456>
2019-03-29 17:55:32 +0800

    7-9 NIO服务器Thread优化-3 SocketChannelAdapter

--- Java/SocketAdvanced/ChatRoom/lib-clink/src/main/java/net/atom/library/clink/impl/IoSelectorProvider.java
+++ Java/SocketAdvanced/ChatRoom/lib-clink/src/main/java/net/atom/library/clink/impl/IoSelectorProvider.java
@@ -2,10 +2,68 @@ package net.atom.library.clink.impl;
 
 import net.atom.library.clink.core.IoProvider;
 
+import javax.swing.*;
 import java.io.IOException;
+import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class IoSelectorProvider implements IoProvider {
+
+    private final AtomicBoolean icClosed = new AtomicBoolean(false);
+    private final Selector readSelector;
+    private final Selector writeSelector;
+
+    private final ExecutorService inputHandlePool;
+    private final ExecutorService outputHanlePool;
+
+    public IoSelectorProvider() throws IOException {
+        this.readSelector = Selector.open();
+        this.writeSelector = Selector.open();
+
+        inputHandlePool =
+                Executors.newFixedThreadPool(4,
+                        new IoProviderThreadFactory("IoProvider-Input-Thread-"));
+
+        outputHanlePool =
+                Executors.newFixedThreadPool(4,
+                        new IoProviderThreadFactory("IoProvider-Input-Thread-"));
+
+        // 开始输入输出的监听
+        startRead();
+        startWrite();
+    }
+
+    private void startRead() {
+        Thread thread = new Thread("Clink IoSelectorProvider ReadSelector Thread") {
+            @Override
+            public void run() {
+                while (!icClosed.get()) {
+
+                }
+            }
+        };
+        thread.setPriority(Thread.MAX_PRIORITY);
+        thread.start();
+    }
+
+    private void startWrite() {
+        Thread thread = new Thread("Clink IoSelectorProvider WriteSelector Thread") {
+            @Override
+            public void run() {
+                while (!icClosed.get()) {
+
+                }
+            }
+        };
+        thread.setPriority(Thread.MAX_PRIORITY);
+        thread.start();
+    }
+
     @Override
     public boolean registerInput(SocketChannel channel, HandleInputCallback callback) {
         return false;
@@ -18,16 +76,40 @@ public class IoSelectorProvider implements IoProvider {
 
     @Override
     public void unRegisterInput(SocketChannel channel) {
-
     }
 
     @Override
     public void unRegisterOutput(SocketChannel channel) {
-
     }
 
     @Override
     public void close() throws IOException {
+    }
 
+    /**
+     * The default thread factory
+     */
+    static class IoProviderThreadFactory implements ThreadFactory {
+
+        private final ThreadGroup group;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final String namePrefix;
+
+        IoProviderThreadFactory(String namePrefix) {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+            this.namePrefix = namePrefix;
+        }
+
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+            if (t.isDaemon()) {
+                t.setDaemon(false);
+            }
+            if (t.getPriority() != Thread.NORM_PRIORITY) {
+                t.setPriority(Thread.NORM_PRIORITY);
+            }
+            return t;
+        }
     }
 }
--- Java/SocketAdvanced/ChatRoom/lib-clink/src/main/java/net/atom/library/clink/impl/SocketChannelAdapter.java
+++ Java/SocketAdvanced/ChatRoom/lib-clink/src/main/java/net/atom/library/clink/impl/SocketChannelAdapter.java
@@ -1,28 +1,110 @@
 package net.atom.library.clink.impl;
 
 import net.atom.library.clink.core.IoArgs;
+import net.atom.library.clink.core.IoProvider;
 import net.atom.library.clink.core.Receiver;
 import net.atom.library.clink.core.Sender;
+import net.atom.library.clink.utils.CloseUtils;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * 发送与接收具体实现
  */
 public class SocketChannelAdapter implements Sender, Receiver, Closeable {
+    private final AtomicBoolean isClosed = new AtomicBoolean(false);
+    private final SocketChannel channel;
+    private final IoProvider ioProvider;
+    private final OnChannelStatusChangedListener listener;
+
+    private IoArgs.IoArgsEventListener receiveIoEventListener;
+    private IoArgs.IoArgsEventListener sendIoEventListener;
+
+    public SocketChannelAdapter(SocketChannel channel, IoProvider ioProvider, OnChannelStatusChangedListener listener) throws IOException {
+        this.channel = channel;
+        this.ioProvider = ioProvider;
+        this.listener = listener;
+        // 设置非阻塞模式
+        channel.configureBlocking(false);
+    }
+
     @Override
     public boolean receiverAsync(IoArgs.IoArgsEventListener listener) throws IOException {
-        return false;
+        if (isClosed.get()) {
+            throw new IOException("Current channel is closed!");
+        }
+
+        receiveIoEventListener = listener;
+
+        return ioProvider.registerInput(channel, inputCallback);
     }
 
     @Override
     public boolean sendAsync(IoArgs args, IoArgs.IoArgsEventListener listener) throws IOException {
-        return false;
+        if (isClosed.get()) {
+            throw new IOException("Current channel is closed");
+        }
+
+        sendIoEventListener = listener;
+        // 当前发送的数据附加到回调中
+        outputCallback.setAttach(args);
+        return ioProvider.registerOutput(channel, outputCallback);
     }
 
     @Override
     public void close() throws IOException {
+        if (isClosed.compareAndSet(false, true)) {
+            ioProvider.unRegisterInput(channel);
+            ioProvider.unRegisterOutput(channel);
+            // 关闭
+            CloseUtils.close(channel);
+            // 回调当前channel已关闭
+            listener.onChannelClosed(channel);
+        }
+    }
 
+
+    private final IoProvider.HandleInputCallback inputCallback = new IoProvider.HandleInputCallback() {
+        @Override
+        protected void canProviderInput() {
+            if (isClosed.get()) {
+                return;
+            }
+            IoArgs args = new IoArgs();
+            IoArgs.IoArgsEventListener listener = SocketChannelAdapter.this.receiveIoEventListener;
+            if (listener != null) {
+                listener.onStarted(args);
+            }
+
+            try {
+                // 具体的读取操作
+                if (args.read(channel) > 0 && listener != null) {
+                    // 读取完成回调
+                    listener.onCompleted(args);
+                } else {
+                    throw new IOException("Cannot read any data!");
+                }
+            } catch (IOException e) {
+                CloseUtils.close(SocketChannelAdapter.this);
+            }
+        }
+    };
+
+    private final IoProvider.HandleOutputCallback outputCallback = new IoProvider.HandleOutputCallback() {
+        @Override
+        protected void canProviderOutput(Object attach) {
+            if (isClosed.get()) {
+                return;
+            }
+            // TODO
+            sendIoEventListener.onCompleted(null);
+        }
+    };
+
+    private interface OnChannelStatusChangedListener {
+        void onChannelClosed(SocketChannel channel);
     }
 }
